e9022601b4d95830bf32787352509f7f3562b95d
Chingpo-Lin <m13823601165@163.com>
2022-04-15 20:47:47 -0700

    initial commit

---
+++ src/main/java/com/webdesign/bestsell/BestsellApplication.java
@@ -0,0 +1,13 @@
+package com.webdesign.bestsell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestsellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestsellApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webdesign/bestsell/BestsellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webdesign.bestsell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestsellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
