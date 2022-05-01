f0f15b6a152c0c941271577734712abb6f1aba3b
darcy521 <honglinya521@gmail.com>
2022-05-01 15:15:15 -0700

    fix some bugs of filter function

--- web/react/src/App.js
+++ web/react/src/App.js
@@ -16,29 +16,29 @@ export default class App extends React.Component {
   }
   sortProducts = (event) => {
     //implement
-    // const sort = event.target.value;
-    // console.log(event.target.value);
-    // // this.setState((state) =>({
-    // //   sort: sort,
-    // //   products: this.state.products.slice().sort((a,b) =>(
-    // //     sort === "lowest"?
-    // //     ((a.price < b.price)? 1:-1):
-    // //     sort === "highest"?
-    // //     ((a.price > b.price)? 1:-1):
-
-    // //   ),)
-    // }));
+    const sort = event.target.value;
+    console.log(event.target.value);
+    this.setState(state => ({
+      sort: sort,
+      products: this.state.products.slice().sort((a,b) =>(
+        sort === "lowest"?
+        ((a.price > b.price)? 1:-1):
+        sort === "highest"?
+        ((a.price < b.price)? 1:-1):
+        ((a.id < b.id)? 1:-1)
+      ))
+    }))
   };
   filterProducts = (event) => {
     //implement
     console.log(event.target.value);
     if(event.target.value === ""){
-      this.setState({size: event.target.value, product:data.products});
+      this.setState({size: event.target.value, products:data.products});
     } else{
       this.setState ({
         size: event.target.value,
         products: data.products.filter(
-          (product) => product.stock.indexOf(event.target.value) >= 0
+          (product) => product.availableSizes.indexOf(event.target.value) >= 0
           ),
       });
     }
--- web/react/src/components/Filter.js
+++ web/react/src/components/Filter.js
@@ -16,12 +16,12 @@ export default class Filter extends Component {
             Filter{" "}
             <select value={this.props.size} onChange={this.props.filterProducts}>
               <option value="">All</option>
-              <option value="0">0</option>
-              <option value="1">1</option>
-              <option value="2">2</option> 
-              <option value="3">3</option> 
-              <option value="4">4</option>
-              <option value="6">6</option>              
+              <option value="XS">XS</option>
+              <option value="S">S</option>
+              <option value="M">M</option> 
+              <option value="L">L</option> 
+              <option value="XL">XL</option>
+              <option value="XXL">XXL</option>              
             </select>              
           </div>
       </div>
