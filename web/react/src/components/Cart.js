ee28463d9f85bd764f32d6abb5e5708be0ff8d96
darcy521 <honglinya521@gmail.com>
2022-05-05 16:13:00 -0700

    add animation-modal

--- web/react/src/components/Cart.js
+++ web/react/src/components/Cart.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import formatCurrency from '../util';
+import Fade from "react-reveal/Fade";
 
 export default class Cart extends Component {
   constructor(props){
@@ -41,6 +42,7 @@ export default class Cart extends Component {
         )}
         <div>
           <div className="cart">
+            <Fade left cascade>
               <ul className="cart-items">
                   {cartItems.map(item =>(
                       <li key={item.id}>
@@ -62,6 +64,7 @@ export default class Cart extends Component {
                       </li>
                   ))}
               </ul>
+              </Fade>
               </div>  
               {cartItems.length !==0 && (
               <div>
@@ -83,6 +86,7 @@ export default class Cart extends Component {
               </div>
               </div>
                 {this.state.showCheckout && (
+                  <Fade right cascade>
                   <div className="cart">
                   <form onSubmit={this.createOrder}>
                     <ul className="form-container">
@@ -118,6 +122,7 @@ export default class Cart extends Component {
                     </ul>
                   </form>
                   </div>
+                  </Fade>
                 )}
                 </div> 
               )}
--- web/react/src/components/Products.js
+++ web/react/src/components/Products.js
@@ -1,15 +1,36 @@
 import React, { Component } from 'react'
 import formatCurrency from '../util'
+import Fade from "react-reveal/Fade";
+import Modal from "react-modal";
+import Zoom from "react-reveal";
 
 export default class Products extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      product: null,
+
+    }
+  }
+
+  openModal = (product) => {
+    this.setState({product});
+  };
+  closeModal = () => {
+    this.setState({product: null});
+  };
+
   render() {
+    const {product} = this.state; 
     return (
       <div>
+        {/* fade animation */}
+        <Fade bottom cascade={true}> 
         <ul className="products">
             {this.props.products.map(product => (
                 <li key={product.id}>
                     <div className="product">
-                        <a href={"#" + product.id}>
+                        <a href={"#" + product.id} onClick={() => this.openModal(product)}>
                             <img src={product.img} alt={product.name}  className="img" />
                             <p>
                                 {product.name}
@@ -26,6 +47,44 @@ export default class Products extends Component {
                 </li>
             ))}
         </ul>
+        </Fade>
+        {product && (
+          <Modal isOpen={true}
+            onRequestClose={this.closeModal}>
+            <Zoom>
+              <button className="close-modal" onClick={this.closeModal}>
+                x
+              </button>
+              <div className="product-details">
+                <img src={product.img} alt={product.name} />
+                <div className="product-details-description">
+                  <p>
+                    <strong>{product.name}</strong>
+                  </p>
+                  <p>
+                    {product.description}
+                  </p>
+                  <p>
+                    Available sizes:{" "}
+                    {product.availableSizes.map((x) => (
+                      <span>
+                        {" "}
+                        <button className="button">{x}</button>
+                      </span>
+                    ))}
+                  </p>
+                  <div className="product-price">
+                      <div>{formatCurrency(product.price)}</div>
+                      <button className="button primary" onClick={() => {
+                        this.props.addToCart(product);
+                        this.closeModal();
+                      }}> Add To Cart</button>
+                  </div>    
+                  </div>  
+              </div>
+            </Zoom>
+          </Modal>
+        )}
       </div>
     )
   }
